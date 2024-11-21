dc05a0475f91842cedad94a26955d017add4f2df
IPMokhov <Jora55524@gmail.com>
2024-11-21 18:40:15 +0300

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -6,3 +6,10 @@ new_age = age + 1
 print(new_age)
 is_student = bool (36>35)
 print(is_student)
+
+
+completed_task = 12
+hours_spent = 1.5
+training_name = "python"
+time_for_one_task = 1.5/12
+print(training_name,'всего задач',completed_task,'затрачено часов',hours_spent,'среднее время выполнения',time_for_one_task)
